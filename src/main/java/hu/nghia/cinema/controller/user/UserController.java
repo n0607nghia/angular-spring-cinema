1f46e3f2ca68680dbe73d932285354ed56173782
n0607nghia <giawuncs@gmail.com>
2026-06-05 17:40:14 +0200

    Enhance `UserController` with `ResponseEntity` for consistent HTTP responses and add request logging

--- src/main/java/hu/nghia/cinema/controller/user/UserController.java
+++ src/main/java/hu/nghia/cinema/controller/user/UserController.java
@@ -2,7 +2,10 @@ package hu.nghia.cinema.controller.user;
 
 import hu.nghia.cinema.dto.UserDto;
 import hu.nghia.cinema.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,31 +14,41 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public List<UserDto> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        logger.info("Fetching all users");
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
-    public UserDto getUserById(@PathVariable Integer id) {
-        return userService.getUserById(id);
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer id) {
+        logger.info("Fetching user by id: {}", id);
+        UserDto userDto = userService.getUserById(id);
+        return userDto != null ? ResponseEntity.ok(userDto) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public UserDto createUser(@RequestBody UserDto userDto) {
-        return userService.createUser(userDto);
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        logger.info("Creating new user: {}", userDto.getUsername());
+        return ResponseEntity.status(201).body(userService.createUser(userDto));
     }
 
     @PutMapping("/{id}")
-    public UserDto updateUser(@PathVariable Integer id, @RequestBody UserDto userDto) {
-        return userService.updateUser(id, userDto);
+    public ResponseEntity<UserDto> updateUser(@PathVariable Integer id, @RequestBody UserDto userDto) {
+        logger.info("Updating user with id: {}", id);
+        UserDto updatedUser = userService.updateUser(id, userDto);
+        return updatedUser != null ? ResponseEntity.ok(updatedUser) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        logger.info("Deleting user with id: {}", id);
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
